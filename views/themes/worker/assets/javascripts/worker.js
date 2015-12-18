fe9b97ad9840b06a3133237152f78233dde146a2
jason weng <wengjianm@gmail.com>
2015-12-18 19:20:05 +0800

    adjust worker layout: new and edit page

---
+++ views/themes/worker/assets/javascripts/worker.js
@@ -0,0 +1,16 @@
+$(function () {
+
+  'use strict';
+
+  $(document).on('click.qor.worker.trigger', '.qor-sub-nav-trigger', function (e) {
+    var terget = $(e.currentTarget);
+    var targetParent = terget.parent();
+
+    targetParent.addClass('current');
+    $('.qor-worker-form-list').not('current').find('form').addClass('hidden');
+    terget.next('form').toggleClass('hidden');
+  });
+
+});
+
+//# sourceMappingURL=worker.js.map
