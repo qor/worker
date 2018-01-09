edcdcca0987a26517c40368ff2801792b7fa60cd
Jinzhu <wosmvp@gmail.com>
2018-01-09 17:51:58 +0800

    Initialize kubernetes backend with k8s SDK

--- queue/kubernetes/kubernetes.go
+++ queue/kubernetes/kubernetes.go
@@ -1,15 +1,41 @@
 package kubernetes
 
+import (
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
 // Kubernetes implemented a worker Queue based on kubernetes jobs
 type Kubernetes struct {
-	Config *Config
+	Clientset *Clientset
+	Config    *Config
 }
 
 // Config kubernetes config
 type Config struct {
+	ClusterConfig *rest.Config
 }
 
 // New initialize Kubernetes
-func New(config *Config) *Kubernetes {
-	return &Kubernetes{Config: config}
+func New(config *Config) (*Kubernetes, error) {
+	var err error
+
+	if config == nil {
+		config = &Config{}
+	}
+
+	if config.ClusterConfig == nil {
+		config.ClusterConfig, err = rest.InClusterConfig()
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	clientset, err := kubernetes.NewForConfig(config.ClusterConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Kubernetes{Clientset: clientset, Config: config}, nil
 }
