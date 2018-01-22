52363d64ae7caff6c17e0d83b013a69a2daa63db
Jinzhu <wosmvp@gmail.com>
2018-01-22 13:56:43 +0800

    Get current running pod

--- queue/kubernetes/kubernetes.go
+++ queue/kubernetes/kubernetes.go
@@ -3,6 +3,7 @@ package kubernetes
 import (
 	"errors"
 	"fmt"
+	"net"
 	"os"
 	"path/filepath"
 
@@ -10,6 +11,7 @@ import (
 	"k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
@@ -51,6 +53,21 @@ func New(config *Config) (*Kubernetes, error) {
 	}
 
 	return &Kubernetes{Clientset: clientset, Config: config}, nil
+}
+
+// GetCurrentPod get current pod
+func (k8s *Kubernetes) GetCurrentPod() *corev1.Pod {
+	var (
+		podlist, err = k8s.Clientset.Core().Pods("").List(metav1.ListOptions{})
+		localeIP     = GetLocalIP()
+	)
+
+	for _, item := range podlist.Items {
+		if item.Status.PodIP == localeIP {
+			return &item
+		}
+	}
+	return nil
 }
 
 // Add a job to k8s queue
@@ -115,4 +132,21 @@ func (k8s *Kubernetes) Kill(job worker.QorJobInterface) error {
 func (k8s *Kubernetes) Remove(job worker.QorJobInterface) error {
 	// Don't remove if it is already running
 	return k8s.Clientset.Core().Pods(k8s.Config.Namespace).Delete("job name", &metav1.DeleteOptions{})
+}
+
+// GetLocalIP returns the non loopback local IP of the host
+func GetLocalIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return ""
+	}
+	for _, address := range addrs {
+		// check the address type and if it is not a loopback the display it
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return ""
 }
