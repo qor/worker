e75d4aa953fcda922f4873ddef9228f02a543f01
Jinzhu <wosmvp@gmail.com>
2018-04-17 11:43:51 +0800

    Implement Scanner, Valuer for Frequency

--- frequency.go
+++ frequency.go
@@ -1,6 +1,11 @@
 package worker
 
-import "time"
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+	"time"
+)
 
 // Frequencier frequencier interface
 type Frequencier interface {
@@ -12,6 +17,30 @@ type Frequency struct {
 	ScheduledStartAt *time.Time
 	ScheduledEndAt   *time.Time
 	Interval         time.Duration
+}
+
+// Scan scan frequency value
+func (frequency *Frequency) Scan(value interface{}) error {
+	switch data := value.(type) {
+	case []byte:
+		return json.Unmarshal(data, frequency)
+	case string:
+		return frequency.Scan([]byte(data))
+	case []string:
+		for _, str := range data {
+			if err := frequency.Scan([]byte(str)); err != nil {
+				return err
+			}
+		}
+	default:
+		return errors.New("unsupported data")
+	}
+	return nil
+}
+
+// Value get value of frequency
+func (frequency Frequency) Value() (driver.Value, error) {
+	return json.Marshal(frequency)
 }
 
 // GetFrequency get frequency
